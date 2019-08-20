22c55974b262d4c544eb095bd866f92ae02e3d1d
Dika Adhitama <dika.adhitama@gmail.com>
2019-08-20 15:50:42 +0700

    Add util file

---
+++ java/src/main/java/util/Util.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Util {
+	public static List<Integer> splitToIntList(String input){
+		return Arrays.stream(splitToStringArr(input))
+				.map(Integer::valueOf).collect(Collectors.toList());
+	}
+	
+	public static int[] splitToIntArr(String input) {
+		return Arrays.stream(splitToStringArr(input))
+				.mapToInt(Integer::parseInt).toArray();
+	}
+	
+	public static String[] splitToStringArr(String input) {
+		return input.split(" ");
+	}
+}
